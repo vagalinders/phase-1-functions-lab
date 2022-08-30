92e971d2bf5fbbad9c84a2f46aef20404d655125
Chris Mann <chrispmann@gmail.com>
2022-08-30 14:46:19 -0700

    Finished first function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+    console.log(blocks);
+    const blocksAway = Math.abs(blocks - 42)
+    return blocksAway;
+}
+
+function distanceFromHqInFeet(blocks){
+    //todo
+}
+
+function distanceTravelledInFeet(start, destination){
+    //todo
+}
+
+function calculatesFarePrice(start, destination){
+    //todo
+}
